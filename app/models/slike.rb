3fc8cc819b1f497046312dc9531b6e71b0a574b6
Adnan1921 <Adnan.biber2@gmail.com>
2020-10-03 19:04:06 +0200

    sadsdad

--- app/models/slike.rb
+++ app/models/slike.rb
@@ -1,3 +1,12 @@
 class Slike < ApplicationRecord
   has_one_attached :avatar
+  def thumbnail
+    return self.avatar.variant(resize: '100x100')
+  end
+  def srednja
+    return self.avatar.variant(resize: '500x300')
+  end
+  def velika
+    return self.avatar.variant(resize: '900x600')
+  end
 end
