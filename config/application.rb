868973009c3b741d4fd1e477b7e87bb19e422c53
Adnan1921 <Adnan.biber2@gmail.com>
2020-10-05 19:40:24 +0200

    3

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module Galerija
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.assets.initialize_on_precompile = false # Moved in app class
+        config.active_record.raise_in_transactional_callbacks = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
