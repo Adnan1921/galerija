470d13d9a74029ca20da59af6ddca39d2f215d73
Adnan1921 <Adnan.biber2@gmail.com>
2020-10-05 18:46:08 +0200

    adddaadsad

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_10_04_215454) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -33,6 +36,55 @@ ActiveRecord::Schema.define(version: 2020_10_04_215454) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "cars", force: :cascade do |t|
+    t.text "user_id"
+    t.text "plate"
+    t.text "registration_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "telefon"
+    t.string "datum"
+    t.string "model"
+    t.string "ime_vozila"
+    t.string "ime_vlasnika"
+  end
+
+  create_table "partners", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "user_id"
+    t.text "email"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "radi_ses", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "registracijes", force: :cascade do |t|
+    t.string "car_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "car_exp_date"
+  end
+
+  create_table "registrations", force: :cascade do |t|
+    t.string "car_id"
+    t.string "expire_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "slikes", force: :cascade do |t|
     t.string "naziv_slike"
     t.string "slika"
@@ -40,6 +92,44 @@ ActiveRecord::Schema.define(version: 2020_10_04_215454) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "opis_slike"
+  end
+
+  create_table "to_do_lists", force: :cascade do |t|
+    t.string "task_title"
+    t.text "task_description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "todo_lists", force: :cascade do |t|
+    t.string "task_title"
+    t.text "task_description"
+    t.datetime "task_time"
+    t.binary "difficulty_level"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "prioritet"
+    t.boolean "radi_se"
+  end
+
+  create_table "uradjenos", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.text "partner_id"
+    t.text "name"
+    t.text "phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
